745deaff5b6ef2f76807b1e20e8d911235a5aa8b
carrottechplus <blohzzang@gmail.com>
2023-06-13 12:56:28 +0900

    list-motion 페이지 세팅 및 스타일링

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-const frame = document.querySelector('section');
-const boxs = frame.querySelectorAll('article');
-const prev = document.querySelector('.prev');
-const next = document.querySelector('.next');
+// test.html 관련 js
+// const frame = document.querySelector('section');
+// const boxs = frame.querySelectorAll('article');
+// const prev = document.querySelector('.prev');
+// const next = document.querySelector('.next');
 
-next.addEventListener('click', () => {
-	frame.append(frame.firstElementChild);
-});
+// next.addEventListener('click', () => {
+// 	frame.append(frame.firstElementChild);
+// });
 
-prev.addEventListener('click', () => {
-	frame.prepend(frame.lastElementChild);
-});
+// prev.addEventListener('click', () => {
+// 	frame.prepend(frame.lastElementChild);
+// });
