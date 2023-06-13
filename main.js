04d5b11f2d5d05b998e2e8bbb10de63464c3fe37
carrottechplus <blohzzang@gmail.com>
2023-06-13 13:21:58 +0900

    list-motion 작업 완료

--- main.js
+++ main.js
@@ -11,3 +11,15 @@
 // prev.addEventListener('click', () => {
 // 	frame.prepend(frame.lastElementChild);
 // });
+
+const frame = document.querySelector('.list');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+next.addEventListener('click', () => {
+	frame.append(frame.firstElementChild);
+});
+
+prev.addEventListener('click', () => {
+	frame.prepend(frame.lastElementChild);
+});
